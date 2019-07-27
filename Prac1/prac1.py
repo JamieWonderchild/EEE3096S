9d4e2fde0fc5098179f29b78de75c9347af4e951
Jamie <jamie.aronson00@gmail.com>
2019-07-27 17:36:14 +0200

    finished prac1

--- Prac1/prac1.py
+++ Prac1/prac1.py
@@ -14,20 +14,59 @@ import RPi.GPIO as GPIO
 
 # Logic that you write
 def main():
+    global count
     GPIO.setmode(GPIO.BCM)
-    GPIO.setup(26, GPIO.OUT)
-    GPIO.setup(19, GPIO.OUT)
-    GPIO.setup(13, GPIO.OUT)
-    GPIO.setup(6, GPIO.IN)
-    
-    GPIO.setup(5, GPIO.IN)
+    pinout_list = [13,19,26]
+    pinin_list = [2,3]
+    GPIO.setup(pinout_list, GPIO.OUT,initial=GPIO.LOW)
+    GPIO.setup(pinin_list, GPIO.IN)
+    GPIO.add_event_detect(2, GPIO.RISING,callback=button_increase,bouncetime = 300)
+    GPIO.add_event_detect(3, GPIO.RISING,callback=button_decrease,bouncetime = 300)    
     while True:
         pass
+        
+def button_decrease(channel):
+    global count
+    if count == 0:
+        count = 7
+    else:
+        count = count -1
+    display()
+    
+def button_increase(channel):
+    global count
+    if count == 7:
+        count = 0
+    else:
+        count = count +1
+    display()
+    
+def display():
+    global count
+    print(count)
+    x= format(count, '03b')
+    print("x=",x)
+    if x[2:3] == '1':
+        GPIO.output(26, GPIO.HIGH)
+    else:
+        GPIO.output(26, GPIO.LOW)
+    if x[1:2] == '1':
+        GPIO.output(19, GPIO.HIGH)
+    else:
+        GPIO.output(19, GPIO.LOW)
+    if x[0:1] == '1':
+        GPIO.output(13, GPIO.HIGH)
+    else:
+        GPIO.output(13, GPIO.LOW)    
+    
+        
+    
 
 
 # Only run the functions if 
 if __name__ == "__main__":
     # Make sure the GPIO is stopped correctly
+    count = 0
     try:
         while True:
             main()
